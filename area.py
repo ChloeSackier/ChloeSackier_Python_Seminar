1f08d039d96992eea825c92b9f368acb149be732
ChloeSackier <chloe.sackier@gmail.com>
2015-02-10 00:46:07 -0500

    Area exercise

---
+++ area.py
@@ -0,0 +1,20 @@
+############################
+# Title: Area of a rectangle#
+# Filename:#
+# Original Author:#
+# Most Recent Edit#
+# Last editor:#
+############################
+
+# Vpython file. Explain here what the purpose of the file is. This file calculates
+# area of a rectangle.
+
+# Ingredients
+
+width = 20          # Length, meters
+height = 30          # Length, meters
+
+# Instructions
+
+area=width*height
+print area
