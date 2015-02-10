dff1c3cf490ac675fce64c1f7a0c0a6fdbc806ba
ChloeSackier <chloe.sackier@gmail.com>
2015-02-10 13:27:03 -0500

    Triangulated spheres

---
+++ test.py
@@ -0,0 +1,7 @@
+from visual import *
+sphere(pos = vector(-1,0,0), radius = 0.5, color = color.green)
+arrow (pos=vector(-1,0,0), axis=vector (0, -1, 0), color = color.red)
+sphere(pos = vector(1,1,0), radius = 0.5, color = color.green)
+arrow (pos=vector(1,1,0), axis=vector (-1, 0, 0), color = color.red)
+sphere(pos = vector(1,-1,0), radius = 0.5, color = color.green)
+arrow (pos=vector(1,-1,0), axis=vector (+1, +1, 0), color = color.red)
