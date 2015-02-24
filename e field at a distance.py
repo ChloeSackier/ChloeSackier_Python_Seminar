349a4c5a5eabac15b81224beee4254b64387db28
ChloeSackier <chloe.sackier@gmail.com>
2015-02-23 22:51:58 -0500

    e field at a distance
    bob takes down the soviet

---
+++ e field at a distance.py
@@ -0,0 +1,24 @@
+from math import sqrt, pi
+from const import E0
+k = 1.0 / (4 * pi * E0)
+
+def bob(charge, location):
+    q = charge
+    l = location
+    distance = [None, None, None]
+    distance = [q[0]-l[0], q[1]-l[1], q[2]-l[2]]
+    d = distance
+
+    E_0 = [None, None, None]
+    mag=sqrt((d[0]**2)+d[1]**2+d[2]**2)
+
+    E_0[0] = k * (q[3]/(mag**2)) * (q[0] / mag)
+    E_0[1] = k * (q[3]/(mag**2)) * (q[1] / mag)
+    E_0[2] = k * (q[3]/(mag**2)) * (q[2] / mag)
+
+    E_tot = [None, None, None]
+    E_tot = [E_0[0] , E_0[1] , E_0[2]]
+
+    print 'E =', E_tot, 'N/C'
+
+bob((2, 7, -2, -1), (6, 3, -4000000000))
