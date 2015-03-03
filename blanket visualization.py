bb5f659a09283bace2f5f2aaf81c7582ff77073a
ChloeSackier <chloe.sackier@gmail.com>
2015-03-03 14:29:27 -0500

    Blanket visualization

---
+++ blanket visualization.py
@@ -0,0 +1,7 @@
+from visual import *
+rod = cylinder (pos=(0,0,0), axis = (0, 0, 1.25), radius = 0.05, opacity = 0.35, color=color.green)
+box = box(pos=(0,0,1), length=2, height=2, width=0.01)
+
+
+#using flux=Qenc/E_0, (and finding Qenc as demonstrated in the visualization,) we can find the flux through
+#the area more easily.
